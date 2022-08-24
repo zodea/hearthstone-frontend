e4ddc121d6f34f1870b8d1f0eef2c607920da218
zodea <zd929560638@gmail.com>
2022-08-24 11:47:47 +0800

    build: add lint for codes and commit message
    add prettier,eslint,husky,commitlint,lint-staged

---
+++ commitlint.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  ignores: [(commit) => commit.includes('init')],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-case': [0],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+}
